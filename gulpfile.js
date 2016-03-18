cc7094729d5695c6b30ee6e9ce4793ba6b6752be
Jan Müller <muellerjan19_dev@icloud.com>
2016-03-18 01:33:36 +0100

    Add compression of js and css files to gulpfile
    * Include minified assets in head.jade
    * Move js files to scripts folder
    * Fix height of piwik iframe on smaller devices

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var jade = require('gulp-jade');
 var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 var deLocals = require('./locals_de');
 var enLocals = require('./locals_en')
 
 
-gulp.task('default', ['less', 'templates_de', 'templates_en'], function() {});
+gulp.task('default', ['less', 'templates_de', 'templates_en', 'minify-css', 'compress'], function() {});
 
 gulp.task('less', function() {
   return gulp.src('./less/*.less')
@@ -15,8 +18,28 @@ gulp.task('less', function() {
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('watch', ['less', 'templates_de', 'templates_en'], function() {
-  return gulp.watch(['./less/*.less', './templates/*.jade', './locals_en.js', './locals_de.js'],['templates_de', 'templates_en' ,'less'])
+gulp.task('minify-css', function() {
+  return gulp.src('./public/css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('muellerjan.min.css'))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('compress', ['concat_js'], function() {
+  return gulp.src('./public/js/muellerjan.js')
+    .pipe(uglify())
+    .pipe(rename('muellerjan.min.js'))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('concat_js', function() {
+  return gulp.src('./scripts/*.js')
+    .pipe(concat('muellerjan.js'))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', ['templates_de', 'templates_en' ,'less', 'minify-css', 'compress'], function() {
+  return gulp.watch(['./less/*.less', './templates/*.jade', './locals_en.js', './locals_de.js'],['templates_de', 'templates_en' ,'less', 'minify-css','concat_js', 'compress'])
 })
 
 gulp.task('templates_de', function() {
