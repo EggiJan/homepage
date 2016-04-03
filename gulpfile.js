cecbcf3e6788ee9f9315c3809c9ffd25d1415b82
Jan Müller <muellerjan19_dev@icloud.com>
2016-04-03 13:46:49 +0200

    Fix broken build of js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -5,41 +5,45 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
 var deLocals = require('./locals_de');
 var enLocals = require('./locals_en')
 
 
-gulp.task('default', ['less', 'templates_de', 'templates_en', 'minify-css', 'compress'], function() {});
+gulp.task('default', ['templates_de', 'templates_en', 'compress', 'minify-css'], function() {
+  return gulp.src('./tmp/*')
+    .pipe(clean())
+});
 
 gulp.task('less', function() {
   return gulp.src('./less/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./tmp'));
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('./public/css/style.css')
+gulp.task('concat_js', function() {
+  return gulp.src('./scripts/*.js')
+    .pipe(concat('muellerjan.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('minify-css', ['less'], function() {
+  return gulp.src('./tmp/style.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename('muellerjan.min.css'))
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('compress', ['concat_js'], function() {
-  return gulp.src('./public/js/muellerjan.js')
+  return gulp.src('./tmp/muellerjan.js')
     .pipe(uglify())
     .pipe(rename('muellerjan.min.js'))
     .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('concat_js', function() {
-  return gulp.src('./scripts/*.js')
-    .pipe(concat('muellerjan.js'))
-    .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('watch', ['templates_de', 'templates_en' ,'less', 'minify-css', 'compress'], function() {
-  return gulp.watch(['./less/*.less', './templates/*.jade', './locals_en.js', './locals_de.js'],['templates_de', 'templates_en' ,'less', 'minify-css','compress'])
+gulp.task('watch', ['templates_de', 'templates_en' , 'minify-css', 'compress'], function() {
+  return gulp.watch(['./less/*.less', './templates/*.jade', './locals_en.js', './locals_de.js'],['templates_de', 'templates_en' ,'minify-css','compress'])
 })
 
 gulp.task('templates_de', function() {
