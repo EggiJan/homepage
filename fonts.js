c660f3a3abebc9f217f159eb6accc2e956afbb5c
Jan Müller <muellerjan19_dev@icloud.com>
2021-04-18 13:25:35 +0200

    Remove twitter and bitbucket

--- fonts.js
+++ fonts.js
@@ -1,14 +1,22 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faBook, faCode, faEnvelope, faAngleRight } from '@fortawesome/free-solid-svg-icons'
-import { faTwitterSquare, faBitbucket, faGithubSquare, faXingSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faBook,
+  faCode,
+  faEnvelope,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithubSquare,
+  faXingSquare,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(
   faUser,
   faBook,
   faCode,
   faEnvelope,
-  faTwitterSquare,
-  faBitbucket,
   faGithubSquare,
   faXingSquare,
   faLinkedin,
